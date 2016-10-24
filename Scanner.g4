6db824a1ea2cada2646d70ba983f35fdb8ba3d46
1312359 <1312359@student.hcmus.edu.vn>
2016-10-24 17:06:40 +0700

    Update pass testcase

--- Scanner.g4
+++ Scanner.g4
@@ -1,44 +1,92 @@
 lexer grammar Scanner;
 
 COMMENT
-	: ('//' ~('\r'|'\n')*
-	| '/''*'.*'*''/') -> skip
+	: ('//' .*? '\n' 
+	| '/*' .*? '*/')  -> skip
   	;
-
+ 
 INTLITERAL
   	:  DIGIT+
+  	| '0x'HEX+
   	;
+fragment HEX: (DIGIT | 'a'..'f' | 'A'..'F') ;
 
 CHARLITERAL
 	: '\''LETTER'\''
-	| '\'''\\'.'\''
+	| '\'''\\'('t'|'n'|'\\'|'"')'\''
 	| '\''DIGIT'\''
 	;
 
 STRINGLITERAL
-	: '"' ~('"')* '"'
+	: '"' (ESC|.)*? '"'
 	;
 	
 BOOLEANLITERAL
 	: 'true'
 	| 'false'
 	;
+
+KEYWORD
+	: 'boolean'
+	| 'callout'
+	| 'class'
+	| 'else'
+	| 'if'
+	| 'break'
+	| 'for'
+	| 'int'
+	| 'return'
+	| 'void'
+	| 'continue'
+	;	
 	
 IDENTIFIER 
-  	:'boolean'
-	|'callout'
-	|'class'
-	|'else'
-	|'if'
-	|'int'
-	|'return'
-	|'for'
-	|'break'
-	|'continue'
+  	: LETTER+
+  	| '_' LETTER+
+  	| LETTER+ '_'
+  	| LETTER+ '_' LETTER+
+  	| LETTER+ DIGIT+
+  	| LETTER+ DIGIT+ LETTER+
+  	| LETTER+ IDENTIFIER
+  	;
+OPERATION
+	: '('
+	| ')'
+	| '['
+	| ']'
+	| ','
+	| ';'
+	| '='
+	| '=='
+	| '-'
+	| '+'
+	| '-'
+	| '*'
+	| '/'
+	| '%'
+	| '<'
+	| '<='
+	| '>'
+	| '>='
+	| '!='
+	| '&&'
+	| '||'
+	| '{'
+	| '}'
+	;
+	
+FLOAT
+	: (DIGIT+ ',' DIGIT*
+	| 		 '.' DIGIT+) -> skip
+	;
+	
+NOTIDENTIFIER 
+  	: (LETTER+ '.') -> skip
   	;
 
 fragment LETTER : ('a'..'z' | 'A'..'Z');
 fragment DIGIT  : ('0'..'9');
+fragment ESC : '\\"' | '\\\\' ;
 
 WS
   	:  (' ' | '\t' | '\n' | '\r' | '\f')+ -> skip
--- src/ScannerLexer.java
+++ src/ScannerLexer.java
@@ -4,35 +4,48 @@ import java.io.*;
 
 public class ScannerLexer {
 	public static void main(String[] args) throws Exception {
-			//String option = null;
-			String filename = "H:\\EclipseNeon\\Scanner\\input.txt"; // duong dan den file can scan
-			// filename = args[1];
+			//String option = args[0];
+			String filename = "H:\\EclipseNeon\\Scanner\\input.txt"; // duong dan den file input mac dinh
+			//filename = args[1]; // neu xai tham so ham main
 			FileReader fr = new FileReader(filename);
 			BufferedReader br = new BufferedReader(fr);
 			String s;
 			String inputs = "";
+			String outputs = "";
+			int i = 1;
 			while ((s = br.readLine()) != null) {
                 inputs = inputs + s;
                 inputs = inputs + '\n';
             }
-			
-			ANTLRInputStream input = new ANTLRInputStream(inputs);
-			Scanner lexer = new Scanner(input);
-			String TokenName = null;
-			while(true) {
-				Token token = lexer.nextToken();
-				switch (token.getType()){
-				 case 2: TokenName = "INTLITERAL"; break;
-				 case 3: TokenName = "CHARLITERAL"; break;
-				 case 4: TokenName = "STRINGLITERAL"; break;
-				 case 5: TokenName = "BOOLEANLITERAL"; break;
-				 case 6: TokenName = "IDENTIFIER"; break;
+			try {
+				ANTLRInputStream input = new ANTLRInputStream(inputs);
+				Scanner lexer = new Scanner(input);
+				String TokenName;
+				while(true) {
+					TokenName = "";
+					Token token = lexer.nextToken();
+					switch (token.getType()){
+					 case 2: TokenName = "INTLITERAL: "; break;
+					 case 3: TokenName = "CHARLITERAL: "; break;
+					 case 4: TokenName = "STRINGLITERAL: "; break;
+					 case 5: TokenName = "BOOLEANLITERAL: "; break;
+					 case 7: TokenName = "IDENTIFIER: "; break;
+					}
+					if(token.getType() == Scanner.EOF) {
+						break;
+					}
+					outputs = outputs + i + ' ' + TokenName + token.getText() + '\n';
+					i++;
 				}
-				if(token.getType() == Scanner.EOF) {
-					break;
-				}
-				 System.out.println(TokenName + " : " + token.getText());
-				 TokenName = "";
+			}
+			catch(Exception ex) {
+				outputs = outputs + ex;
+			}
+			finally{
+				FileWriter fw = new FileWriter(filename+".out");
+				System.out.println(outputs);
+				fw.write(outputs);
+				fw.close();
 			}
 	}
 }
